6926d3e8e09be69e5105a4b2996dc4b73efba209
Specter <mikexx1264@gmail.com>
2020-10-21 01:05:46 -0500

    Desing changes / routes / services update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import './App.css';
-import ListOfGifs from './components/ListOfGifs';
-import { Link, Route } from "wouter";
+import React from 'react'
+import './App.css'
+import Home from './pages/Home'
+import SearchResults from './pages/SearchResults'
+import Detail from './pages/Detail'
+
+import { Link, Route } from "wouter"
 
 export default function App() {
   return (
     <div className="App">
       <section className="App-content">
-      <h1>App</h1>
-      <Link to='/gif/overwatch'>Gifs de overwatch</Link>
-      <Link to='/gif/rick'>Gifs de rick</Link>
-      <Link to='/gif/boxer'>Gifs de boxer</Link>
-      <Route 
-        component={ListOfGifs}
-        path="/gif/:keyword" />
+        <Link to="/">
+          <img className="App-logo" alt='GIFpi logo' src='/logo192.png' />
+        </Link>
+        <Route
+          component={Home}
+          path="/"
+        />
+        <Route
+          component={SearchResults}
+          path="/search/:keyword"  />
+        <Route
+          component={Detail}
+          path="/gif/:id"
+        />
       </section>
     </div>
   );
