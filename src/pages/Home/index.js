2cbb24ed5aa79bdecba5e352658a24fb7c6ff0f1
Specter <mikexx1264@gmail.com>
2020-10-22 15:42:51 -0500

    added a seach engine / fix issues

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,27 @@
-import React from "react"
-import { Link } from "wouter"
+import React, {useState} from "react"
+import { Link, useLocation } from "wouter"
 
 const POPULAR_GIFS = ["Matrix", "Overwatch", "Rick", "Dogs"]
 
 export default function Home() {
+  const [keyword, setKeyword] = useState('')
+  const [path, pushLocation] = useLocation()
+
+  const handleSubmit = evt => {
+    evt.preventDefault()
+    //navegar a otra ruta
+    pushLocation(`/search/${keyword}`)
+  }
+
+  const handleChange = env => {
+    setKeyword(env.target.value)
+  }
+
   return (
     <>
+      <form onSubmit={handleSubmit}>
+        <input placeholder="Search a gif here..." onChange={handleChange} type='text' value={keyword} />
+      </form>
       <h3 className="App-title">Los gifs más populares</h3>
       <ul>
       {POPULAR_GIFS.map((popularGif) => (
@@ -16,4 +32,4 @@ export default function Home() {
       </ul>
     </>
   )
-} 
+}
