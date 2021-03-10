526fefb5f428f958df92a7edbed788c3337e724a
Specter <mikexx1264@gmail.com>
2021-03-09 18:38:37 -0600

    Lazyloading trending

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -2,11 +2,34 @@ import React, { useState, useEffect } from 'react'
 import getTrendingTerms from 'services/getTrendingTermsServices'
 import Category from '../Category'
 
-export default function TrendingSearches () {
+function TrendingSearches () {
   const [trends, setTrends] = useState([])
 
   useEffect(function() {
     getTrendingTerms().then(setTrends)
   }, [])
   return <Category name='Tendencias' options={trends} />
+}
+
+export default function LazyTrending () {
+  const [show, setShow] = useState(false)
+
+  useEffect(function () {
+    const onChange = (entries) => {
+      const el = entries[0]
+      if (el.isIntersecting) {
+        setShow(true)
+      }
+    }
+
+    const observer = new IntersectionObserver(onChange, {
+      rootMargin: '100px'
+    })
+
+    observer.observe(document.getElementById('LazyTrending'))
+  })
+
+  return <div id='LazyTrending'>
+    {show ? <TrendingSearches /> : null}
+  </div>
 }
